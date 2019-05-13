854c21ebcf5c9516dba06b8465d5c70e600a7853
CaptainIllidan <37904051+CaptainIllidan@users.noreply.github.com>
2019-05-13 18:00:04 +0400

    done

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -8,13 +8,31 @@ using System.Threading.Tasks;
 
 namespace Filters.Controllers
 {
-    [Profile]
-    public class HomeController:Controller
+    [Message("This is the Controller-Scoped Filter", Order =10)]
+    public class HomeController : Controller
     {
-        public IActionResult Index()=>View("Message",
+        [Message("This is the First Action-Scoped Filter", Order =1)]
+        [Message("This is the Second Action-Scoped Filter", Order =-1)]
+        public IActionResult Index() => View("Message",
                     "This is the Index action on the Home controller");
 
-        public IActionResult SecondAction()=>View("Message",
+        public IActionResult SecondAction() => View("Message",
                     "This is the SecondAction action on the Home controller");
+
+        public ViewResult GenerateException(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentException(nameof(id));
+            }
+            else if (id > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            else
+            {
+                return View("Message", "This value is {id}");
+            }
+        }
     }
 }
--- Filters/Infrastructure/ProfileAttribute.cs
+++ Filters/Infrastructure/ProfileAttribute.cs
@@ -10,14 +10,23 @@ namespace Filters.Infrastructure
 {
     public class ProfileAttribute:ActionFilterAttribute
     {
+        private Stopwatch timer;
+        private double actionTime;
+
         public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            Stopwatch timer = Stopwatch.StartNew();
+            timer = Stopwatch.StartNew();
 
             await next();
 
+            actionTime = timer.Elapsed.TotalMilliseconds;
+        }
+
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        { 
             timer.Stop();
-            string result = "<div>Elapsed time: "
+            string result = "<div>Action time: "
+                + $"{actionTime} ms </div><div> Total time:"
                 + $"{timer.Elapsed.TotalMilliseconds} ms</div>";
             byte[] bytes = Encoding.ASCII.GetBytes(result);
             await context.HttpContext.Response.Body.WriteAsync(bytes, 0, bytes.Length);
